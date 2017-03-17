ebbfdd44049edbc66f85b4564b96787a3288a2df
cryptanx <cryptanx@gmail.com>
2017-03-17 17:04:22 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # extract_attachment_from_email
+
+extract attachment from .eml file.
---
+++ extract_attachment_from_email.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+import csv
+import os
+from email.parser import Parser
+import hashlib
+import email.mime.text
+import email.header
+
+DIRECTORY = 'msg'
+ATTACHMENT_DIR= 'attach'
+# DIRECTORY = 'msg_test'
+OUTPUT_FILE = 'test_result.csv'
+ERROR_LOG = 'error_log.csv'
+
+def get_all_files(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            yield os.path.join(root, file)
+
+
+def export_to_csv(result, output_file):
+    with open(output_file, 'a') as f:
+        writecsv = csv.writer(f)
+        writecsv.writerow(result)
+
+
+def get_attachment_file(file):
+    ''' Return the list of mail attachment filename and sha1
+
+        >>> get_attachment_file(file)
+        (('aaa.exe', 'bbb.exe'), ('qazwsxedcrqazwsxedcrqazwsxedcrqazwsxedcr', 'rfvtgbyhnurfvtgbyhnurfvtgbyhnurfvtgbyhnu'))
+    '''
+    email_filename_list = []
+    filename_list = []
+    sha1_list = []
+    msg = email.message_from_file(open(file))
+    attachments = msg.get_payload()
+    if not os.path.exists(ATTACHMENT_DIR):
+        os.mkdir(ATTACHMENT_DIR)
+    email_filename_list.append(file)
+    for attachment in attachments:
+        try:
+            fnam = attachment.get_filename()
+            f = open(ATTACHMENT_DIR + '/' + fnam, 'wb').write(attachment.get_payload(decode=True,))
+#             print (fnam)
+            filename_list.append(fnam)
+            # sha1 = hashlib.sha1(f).hexdigest()
+            # sha1_list.append(sha1)
+            f.close()
+        except Exception as detail:
+            pass
+
+    # return (email_filename_list, filename_list, sha1_list)
+    return (email_filename_list, filename_list)
+
+
+if __name__ == '__main__':
+    error_list = []
+    for file in get_all_files(DIRECTORY):
+        with open(file) as fp:
+            contents = []
+            contents.append(file)
+            headers = Parser().parse(fp)
+            contents = get_attachment_file(file)
+            try:
+                export_to_csv(contents, OUTPUT_FILE)
+            except:
+                error_list.append(file)
+                export_to_csv(error_list, ERROR_LOG)
+
+#     with open('error_list.csv', 'a') as f:
+#        f.write(error_list)
